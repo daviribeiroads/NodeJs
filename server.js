7b5f75eeb083fc86d65955107d87e9cae5b9d26c
daviribeiroads <analiseds2022@gmail.com>
2025-03-23 13:33:32 -0300

    Esse servidor feito com o node foi criado apenas para teste

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-//Criando um servidor para rodar minha aplicação
+//Como criar um servidor com node para rodar minha aplicação
 import { createServer } from "node:http";
 
-const server = createServer(() => {
-    console.log("Oi");
+const server = createServer((request, response) => {
+    response.write("Hello word!")
+
+    return response.end()
 })
 
 //Esse metodo eu passo a porta para minha aplicação executar
