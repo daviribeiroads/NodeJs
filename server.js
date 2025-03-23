5a12f570c1ffe044565eef46b63ad05a04f86989
daviribeiroads <analiseds2022@gmail.com>
2025-03-23 13:57:43 -0300

    Instalei o framework fastify e criei um server na port 3333

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
-//Como criar um servidor com node para rodar minha aplicação
-import { createServer } from "node:http";
+// //Como criar um servidor com node para rodar minha aplicação
+// import { createServer } from "node:http";
 
-const server = createServer((request, response) => {
-    response.write("Hello word!")
+// const server = createServer((request, response) => {
+//     response.write("Hello word!")
 
-    return response.end()
+//     return response.end()
+// })
+
+// //Esse metodo eu passo a porta para minha aplicação executar
+// server.listen(3333)
+
+// Criando servidor com o fastify
+import { fastify } from "fastify";
+
+const server = fastify();
+
+server.listen({
+    port: 3333,
 })
-
-//Esse metodo eu passo a porta para minha aplicação executar
-server.listen(3333)
