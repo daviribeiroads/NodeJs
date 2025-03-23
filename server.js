2078b34a4f3e36b6c4979f9754c5fa09f3150928
daviribeiroads <analiseds2022@gmail.com>
2025-03-23 14:53:09 -0300

    Criado um banco em memory, apenas para testar a API

--- server.js
+++ server.js
@@ -1,17 +1,6 @@
-// //Como criar um servidor com node para rodar minha aplicação
-// import { createServer } from "node:http";
-
-// const server = createServer((request, response) => {
-//     response.write("Hello word!")
-
-//     return response.end()
-// })
-
-// //Esse metodo eu passo a porta para minha aplicação executar
-// server.listen(3333)
-
 // Criando servidor com o fastify
 import { fastify } from "fastify";
+import { DatabaseMemory } from "./database-memory.js";
 
 const server = fastify();
 
